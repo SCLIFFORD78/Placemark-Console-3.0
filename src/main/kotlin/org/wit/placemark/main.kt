c8da716d849057d5fcc9162b6fec4a968cf71585
SCLIFFORD78 <faugh01@gmail.com>
2021-09-16 00:51:45 +0100

    no message

--- src/main/kotlin/org/wit/placemark/main.kt
+++ src/main/kotlin/org/wit/placemark/main.kt
@@ -5,5 +5,6 @@ import mu.KotlinLogging
 private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>){
+    logger.info { "Launching Placemark Console App" }
     println("Placemark Kotlin App Version 1.0")
 }
