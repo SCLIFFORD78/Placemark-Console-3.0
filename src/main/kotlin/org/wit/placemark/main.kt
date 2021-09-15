7e371de1f10336f8d9538c77e01d74c5263f6f1c
SCLIFFORD78 <faugh01@gmail.com>
2021-09-16 00:49:23 +0100

    no message

---
+++ src/main/kotlin/org/wit/placemark/main.kt
@@ -0,0 +1,9 @@
+package org.wit.placemark
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>){
+    println("Placemark Kotlin App Version 1.0")
+}
